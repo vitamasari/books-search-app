8c9c1ace89ce91af7a3c63dfa4e87ead701c1868
vitamasari <vitamasari88@gmail.com>
2025-07-11 22:58:54 +0700

    Create books_spider.py

---
+++ scrapy_project/spiders/books_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    start_urls = ['https://books.toscrape.com/']
+
+    def parse(self, response):
+        for book in response.css('article.product_pod'):
+            yield {
+                'title': book.css('h3 a::attr(title)').get(),
+                'price': book.css('p.price_color::text').get(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
